d9991358e113482e884cd12f603ddbecb54bdbbe
Rui Zhouhao <976667412@qq.com>
2020-09-29 10:48:23 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,16 @@
 const aCar = {
   owner : "Joe Bloggs",
+  address : '3 Walkers Lane',
+  previous_owners: [
+    {
+      name: 'Pat Smith',
+      address: '1 Main Street'
+    },
+    {
+      name: 'Sheila Dwyer',
+      address: '2 High Street'
+    }
+  ],
   type : {
     make: 'Toyota',
     model: 'Corolla',
@@ -28,3 +39,7 @@ aCar.color = {
 };
 
 console.log('It is a ' + aCar.color.exterior + ' car,' + ' ' + aCar.mileage + ' mileage,' + ' with ' + aCar.color.interior.texture + ' interior')
+
+// Arrays
+// console.log('First owner : ' + aCar.previous_owners[0] )
+console.log('First owner: ' + aCar.previous_owners[0].name + ' - ' + aCar.previous_owners[0].address)
