83153f27a9bf87482f95c8b4d59082541510f97e
Rui Zhouhao <976667412@qq.com>
2020-09-29 10:33:27 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -15,3 +15,16 @@ const aCar = {
 console.log(aCar.owner);
 console.log(aCar.owner + ' drives a ' + aCar.type.make);
 console.log('Reg. = ' + aCar.registration.year + '-' + aCar.registration.country_code + '-' + aCar.registration.number);
+
+// dynamic_properties
+aCar.mileage = 10000;
+
+aCar.color = {
+  exterior: 'red',
+  interior: {
+    texture: 'leather',
+    shade: 'cream'
+  }
+};
+
+console.log('It is a ' + aCar.color.exterior + ' car,' + ' ' + aCar.mileage + ' mileage,' + ' with ' + aCar.color.interior.texture + ' interior')
